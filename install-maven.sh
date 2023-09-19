54d28efc235738f7e4e7c6a5b12ff6c314399bc7
lucasdk3 <lucas.batista@midway.com.br>
2023-09-19 08:17:49 -0300

    fix

--- install-maven.sh
+++ install-maven.sh
@@ -1,36 +1,8 @@
 #!/bin/sh
 set -e
 
-sudo apt-get update
-sudo apt install default-jdk
+sudo apt-get -u upgrade -y
+sudo apt install openjdk-17-jdk -y
 java -version
-sudo apt-get -y install maven
+sudo apt-get -y install maven -y
 mvn -version
-# script to install maven
-
-# todo: add method for checking if latest or automatically grabbing latest
-# mvn_version=$(curl https://maven.apache.org/download.cgi --silent | grep "Downloading Apache Maven " | grep -oP '[0-9].[0-9]+.[0-9]+')
-# url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
-# install_dir="/opt/maven"
-
-# if [ -d ${install_dir} ]; then
-#     mv ${install_dir} ${install_dir}.$(date +"%Y%m%d")
-# fi
-
-# mkdir ${install_dir}
-# curl -fsSL ${url} | tar zx --strip-components=1 -C ${install_dir}
-
-# cat << EOF > /etc/profile.d/maven.sh
-# #!/bin/sh
-# export MAVEN_HOME=${install_dir}
-# export M2_HOME=${install_dir}
-# export M2=${install_dir}/bin
-# export PATH=${install_dir}/bin:$PATH
-# EOF
-
-# source /etc/profile.d/maven.sh
-
-# echo maven installed to ${install_dir}
-# mvn --version
-
-# printf "\n\nTo get mvn in your path, open a new shell or execute: source /etc/profile.d/maven.sh\n"
