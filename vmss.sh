44a9e3fcaebeb926c29ff5682f27cf86d4a2373d
lucasdk3 <lucas.batista@midway.com.br>
2023-09-19 10:37:03 -0300

    fix

--- vmss.sh
+++ vmss.sh
@@ -4,7 +4,7 @@ az vmss extension set \
   --name CustomScript \
   --resource-group RG-PEPSICO-SHARED \
   --vmss-name private-azure-pool \
-  --settings '{"fileUris": ["https://raw.githubusercontent.com/lucasdk3/agents-scripts/main/install-maven.sh"],"commandToExecute": "./install-maven.sh"}'
+  --settings '{"fileUris": ["https://raw.githubusercontent.com/lucasdk3/agents-scripts/main/install-maven.sh"],"commandToExecute": "/bin/bash install-maven.sh"}'
 
   az vmss extension delete \
     --name CustomScript \
