9b76993178d433e8760f34ff0dde92c5169f96c4
lucasdk3 <lucas.batista@midway.com.br>
2023-09-19 14:20:30 -0300

    teste

--- install-maven.sh
+++ install-maven.sh
@@ -1,34 +1,33 @@
 #!/bin/sh
 set -e
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt-get -u upgrade -y
 sudo apt-get install default-jdk -y
-java -version
-TMP_MAVEN_VERSION=3.9.4
-cd /tmp; wget https://apache.org/dist/maven/maven-3/$TMP_MAVEN_VERSION/binaries/apache-maven-$TMP_MAVEN_VERSION-bin.tar.gz -P /tmp
-sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
-sudo rm /tmp/apache-maven-*-bin.tar.gz
-install_dir="/opt/maven"
-if [ -d "/opt/maven" ]; then
-    sudo rm -rf /opt/maven
-fi
-sudo ln -s /opt/apache-maven-$TMP_MAVEN_VERSION /opt/maven
-sudo touch /etc/profile.d/maven.sh
-ls -l /etc/profile.d/maven.sh
-echo $USER
-echo $user
-sudo chown -R /etc/profile.d/maven.sh
+# java -version
+# TMP_MAVEN_VERSION=3.9.4
+# cd /tmp; wget https://apache.org/dist/maven/maven-3/$TMP_MAVEN_VERSION/binaries/apache-maven-$TMP_MAVEN_VERSION-bin.tar.gz -P /tmp
+# sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
+# sudo rm /tmp/apache-maven-*-bin.tar.gz
+# install_dir="/opt/maven"
+# if [ -d "/opt/maven" ]; then
+#     sudo rm -rf /opt/maven
+# fi
+# sudo ln -s /opt/apache-maven-$TMP_MAVEN_VERSION /opt/maven
+# sudo touch /etc/profile.d/maven.sh
+# ls -l /etc/profile.d/maven.sh
+# echo $USER
+# echo $user
+# sudo chown -R /etc/profile.d/maven.sh
 
-sudo bash -c 'cat << EOF > /etc/profile.d/maven.sh
-export JAVA_HOME=/usr/lib/jvm/default-java
-export M2_HOME=/opt/maven
-export MAVEN_HOME=/opt/maven
-export PATH=/opt/maven/bin:$PATH
-EOF'
+# sudo bash -c 'cat << EOF > /etc/profile.d/maven.sh
+# export JAVA_HOME=/usr/lib/jvm/default-java
+# export M2_HOME=/opt/maven
+# export MAVEN_HOME=/opt/maven
+# export PATH=/opt/maven/bin:$PATH
+# EOF'
 
-sudo chmod +x /etc/profile.d/maven.sh
-source /etc/profile.d/maven.sh
-mvn -v
+# sudo chmod +x /etc/profile.d/maven.sh
+# source /etc/profile.d/maven.sh
+# mvn -v
 
 # # Install Docker CE
 # sudo apt-get -u upgrade -y
