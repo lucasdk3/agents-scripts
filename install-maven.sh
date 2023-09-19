2c2822be57dced0b69aa9fa56fca05f2e9020481
lucasdk3 <lucas.batista@midway.com.br>
2023-09-19 09:45:48 -0300

    ajustado scripts

--- install-maven.sh
+++ install-maven.sh
@@ -2,7 +2,50 @@
 set -e
 
 sudo apt-get -u upgrade -y
-sudo apt install openjdk-17-jdk -y
+sudo apt install default-jdk -y
 java -version
-sudo apt-get -y install maven -y
-mvn -version
+TMP_MAVEN_VERSION=3.9.4
+cd /tmp; wget https://apache.org/dist/maven/maven-3/$TMP_MAVEN_VERSION/binaries/apache-maven-$TMP_MAVEN_VERSION-bin.tar.gz -P /tmp
+sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
+sudo rm /tmp/apache-maven-*-bin.tar.gz
+install_dir="/opt/maven"
+if [ -d "/opt/maven" ]; then
+    sudo rm -rf /opt/maven
+fi
+sudo ln -s /opt/apache-maven-$TMP_MAVEN_VERSION /opt/maven
+sudo touch /etc/profile.d/maven.sh
+sudo chown $USER /etc/profile.d/maven.sh
+
+sudo bash -c 'cat << EOF > /etc/profile.d/maven.sh
+export JAVA_HOME=/usr/lib/jvm/default-java
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=/opt/maven/bin:$PATH
+EOF'
+
+sudo chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+mvn -v
+
+# Install Docker CE
+sudo apt-get update -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+sudo echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
---
+++ vmss.sh
@@ -0,0 +1,7 @@
+az vmss extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --resource-group RG-PEPSICO-SHARED \
+  --vmss-name private-azure-pool \
+  --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/lucasdk3/agents-scripts/main/install-maven.sh"],"commandToExecute": "./install-maven.sh"}'
