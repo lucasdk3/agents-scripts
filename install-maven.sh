5256027ea3d97ddecb7d0524855daba559e2541f
lucasdk3 <lucas.batista@midway.com.br>
2023-09-19 18:31:46 -0300

    fix

--- install-maven.sh
+++ install-maven.sh
@@ -47,22 +47,22 @@ sudo chown -R root /etc/profile.d/maven.sh
 
 python3 -m pip install PyYAML
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# sudo echo \
-# "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-# $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key | apt-key add -"
+sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key | apt-key add -"
 
-# sudo add-apt-repository http://dl.openfoam.org/ubuntu
+sudo add-apt-repository http://dl.openfoam.org/ubuntu
 
-# sudo apt-get update -y
+sudo apt-get update -y
 
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
-# # Install docker-compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
-#     -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose 
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose 
