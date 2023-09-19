26e6f519552295bd0dc37dbfb7fc72d5ea14f02b
lucasdk3 <lucas.batista@midway.com.br>
2023-09-19 13:48:15 -0300

    fix

--- install-maven.sh
+++ install-maven.sh
@@ -30,14 +30,13 @@ source /etc/profile.d/maven.sh
 mvn -v
 
 # Install Docker CE
-sudo apt-get update -y --fix-missing
+sudo apt-get update -y
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release \
-    --fix-missing
+    lsb-release
     
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -45,7 +44,7 @@ sudo echo \
 "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io --fix-missing
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
 # Install docker-compose
